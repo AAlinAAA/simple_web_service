4d34bd53fed2629327b4c6105e08e15c476d9419
AlinaandRomansLab <94863617+AAlinAAA@users.noreply.github.com>
2023-03-17 23:36:27 +0300

    Initial commit

---
+++ src/main/java/solutions/javasoft/controller/ProductController.java
@@ -0,0 +1,20 @@
+package solutions.javasoft.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import solutions.javasoft.dao.model.Product;
+import solutions.javasoft.dao.repository.ProductRepository;
+
+@RequestMapping("product")
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+}
---
+++ src/main/java/solutions/javasoft/dao/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package solutions.javasoft.dao.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import solutions.javasoft.dao.model.Product;
+
+public interface ProductRepository extends CrudRepository<Product, String> {
+    Iterable<Product> findByName(String name);
+
+}
